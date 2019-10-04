a296048a0fb51113751fe745fab7b60f9c4975bd
Donghao Wu <donghao.wu.us@gmail.com>
2019-10-04 15:25:36 -0400

    ready for deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //library
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path')
 //apply
 const app = express();
 connectDB();
@@ -16,5 +16,13 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/portfolio', require('./routes/api/portfolio'));
 app.use('/api/transactions', require('./routes/api/transactions'));
 
+//Serve static  assets in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 //active the server
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
