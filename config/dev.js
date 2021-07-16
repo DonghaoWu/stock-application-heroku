4b18b2a078cad20524e6368b910b8b5ed6b2b01c
Donghao Wu <donghao.wu.us@gmail.com>
2021-07-16 16:22:17 -0400

    add security.

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://Donghao:I0pxzLf3HYQGAQKf@cluster0-0cgmm.mongodb.net/test?retryWrites=true&w=majority',
-  jwtSecret: 'final-202107-edition-secret',
-  finhubApi: '5F53S1QWA484BWTH',
-};
